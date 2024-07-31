6ca451bd0b7ebd7e884980647e59f06f4d11d081
Reyan <reyanjohnnarvaja@gmail.com>
2024-08-01 03:34:14 +0800

    added hero

--- app/page.js
+++ app/page.js
@@ -1,10 +1,25 @@
 
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-   fwefwe
+      <div className={styles.hero}>
+        <div>
+          
+        </div>
+        <div>
+          <div className={styles.heroCTA}>
+            <h2>New Arrival</h2>
+            <h1>Discover Our <br/>New Collection</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+            <Link className={styles.heroCTABtn} href={'/shop'}>BUY NOW</Link>
+          </div>
+        </div>
+      </div>
+
+      
     </main>
   );
 }
